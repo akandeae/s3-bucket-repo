6340589049b6fc8a16260ce79d5a76a38a3119c7
akandeae <akande.ae@gmail.com>
2024-06-20 11:09:57 -0500

    updated

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
-provider "aws" {
-  region = "us-east-1" # Change the region if needed
+
+
+# Define the required Terraform version
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired region
+}
+
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
